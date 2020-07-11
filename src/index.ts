95162894d526d237ebc2e55cf3c355a37681354d
Amish Shah <amishshah.2k@gmail.com>
2020-07-11 14:16:39 +0100

    chore: create docker-compose

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
-export function sum(a: number, b: number): number {
-	return a + b;
-}
+import 'reflect-metadata';
+import { createConnection, getRepository } from 'typeorm';
+import { User, AccountType, AccountStatus } from './entity/User';
+
+createConnection({
+	type: 'postgres',
+	host: 'localhost',
+	port: 5432,
+	username: 'unics_social',
+	password: 'password123',
+	database: 'unics_social',
+	entities: [
+		User
+	],
+	synchronize: true,
+	logging: false
+}).then(async connection => {
+	const repo = await connection.getRepository(User);
+	const user = new User();
+	user.forename = 'Amish';
+	user.surname = 'Shah';
+	user.accountType = AccountType.ADMIN;
+	user.accountStatus = AccountStatus.VERIFIED;
+	user.email = 'amishshah.2k@gmail.com';
+	await repo.save(user);
+}).catch(console.log);
+
