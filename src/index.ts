271478834bbe1cde729a3c895200cbe4a1785e43
Amish Shah <amishshah.2k@gmail.com>
2020-07-11 17:41:03 +0100

    refactor: tidy up index file

--- src/index.ts
+++ src/index.ts
@@ -5,27 +5,17 @@ import { config as loadEnv } from 'dotenv';
 loadEnv();
 
 import { createConnection } from 'typeorm';
-import { User, AccountType, AccountStatus } from './entities/User';
 import { getConfig } from './util/config';
+import { User } from './entities/User';
 
-createConnection({
-	type: 'postgres',
-	...getConfig().db, // username, password, host, port, database
-	entities: [
-		User
-	],
-	synchronize: true,
-	logging: false
-}).then(async connection => {
-	console.log('connected!');
-	const repo = await connection.getRepository(User);
-	const user = new User();
-	user.forename = 'John';
-	user.surname = 'Doe';
-	user.password = 'Test';
-	user.accountType = AccountType.USER;
-	user.accountStatus = AccountStatus.VERIFIED;
-	user.email = 'tes1t@email.com';
-	await repo.save(user);
-	console.log('saved!');
-}).catch(console.log);
+export async function createApp() {
+	await createConnection({
+		type: 'postgres',
+		...getConfig().db, // username, password, host, port, database
+		entities: [
+			User
+		],
+		synchronize: true,
+		logging: false
+	});
+}
