3cada7204496598f663ba5bb2674185f39763e5f
Amish Shah <amishshah.2k@gmail.com>
2020-07-12 22:12:11 +0100

    test: add additional UserController test

--- tests/integration/controllers/userController.test.ts
+++ tests/integration/controllers/userController.test.ts
@@ -1,6 +1,6 @@
 import { UserService } from '../../../src/services/UserService';
 import { createApp } from '../../../src';
-import { mock, instance, when, anything, verify, objectContaining } from 'ts-mockito';
+import { mock, instance, when, anything, verify, objectContaining, reset } from 'ts-mockito';
 import { container } from 'tsyringe';
 import supertest from 'supertest';
 import '../../util/dbTeardown';
@@ -14,6 +14,10 @@ beforeAll(async () => {
 	container.register<UserService>(UserService, { useValue: instance(mockedUserService) });
 
 	app = await createApp();
+});
+
+beforeEach(() => {
+	reset(mockedUserService);
 });
 
 describe('UserController', () => {
@@ -37,7 +41,7 @@ describe('UserController', () => {
 			when(mockedUserService.registerUser(anything())).thenReject();
 			const res = await supertest(app).post('/api/v1/register');
 			verify(mockedUserService.registerUser(anything())).called();
-			expect(res.status).toEqual(400);
+			expect(res.status).toBeGreaterThanOrEqual(400);
 		});
 	});
 
@@ -54,7 +58,14 @@ describe('UserController', () => {
 			when(mockedUserService.verifyUser(anything())).thenReject();
 			const res = await supertest(app).get('/api/v1/verify?confirmationId=3');
 			verify(mockedUserService.verifyUser('3')).called();
-			expect(res.status).toEqual(400);
+			expect(res.status).toBeGreaterThanOrEqual(400);
+		});
+
+		test('Gives error when confirmation id not provided', async () => {
+			when(mockedUserService.verifyUser(anything())).thenResolve();
+			const res = await supertest(app).get('/api/v1/verify');
+			verify(mockedUserService.verifyUser(anything())).never();
+			expect(res.status).toBeGreaterThanOrEqual(400);
 		});
 	});
 });
