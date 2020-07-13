e50d7101a3607c70d6b672719a2db7348fdd7687
Amish Shah <amishshah.2k@gmail.com>
2020-07-13 22:09:17 +0100

    refactor: specify payload type for JWT

--- src/util/auth.ts
+++ src/util/auth.ts
@@ -1,15 +1,16 @@
 import { sign, verify as _verify } from 'jsonwebtoken';
 import { getConfig } from './config';
 import { promisify } from 'util';
+import { User } from '../entities/User';
 
-type Payload = Record<string, string>;
+type Payload = Pick<User, 'id'>;
 
 const verify = promisify(_verify);
 
 export function generateJWT(payload: Payload): Promise<string> {
 	// wouldn't play nice with promisify :(
 	return new Promise((resolve, reject) => {
-		sign(payload, getConfig().jwtSecret, { expiresIn: '3 days' }, (err, jwt) => {
+		sign({ id: payload.id }, getConfig().jwtSecret, { expiresIn: '3 days' }, (err, jwt) => {
 			err ? reject(err) : resolve(jwt);
 		});
 	});
--- tests/unit/util/auth.test.ts
+++ tests/unit/util/auth.test.ts
@@ -1,15 +1,16 @@
 import { generateJWT, verifyJWT } from '../../../src/util/auth';
 
 describe('AuthUtil', () => {
-	test('Payload can be signed then verified', async () => {
-		const payload = { test: 'passes, hopefully' };
+	test('Payload can be signed then verified, removing any extra fields', async () => {
+		const payload = { id: '1234567890abcdef', thisShouldBe: 'removed when generating the JWT' };
 		const token = await generateJWT(payload);
-		const reformedPayload = await verifyJWT(token);
-		expect(reformedPayload).toMatchObject(payload);
+		const reformedPayload = await verifyJWT(token) as any;
+		expect(reformedPayload.id).toEqual(payload.id);
+		expect(reformedPayload.thisShouldBe).toBeUndefined();
 	});
 
 	test('Throws when invalid JWT attempted to be verified', async () => {
-		const payload = { test: 'passes, hopefully' };
+		const payload = { id: '1234567890abcdef' };
 		const token = await generateJWT(payload);
 		expect(verifyJWT(`${token}1`)).rejects.toThrow();
 		expect(verifyJWT(`1${token}`)).rejects.toThrow();
