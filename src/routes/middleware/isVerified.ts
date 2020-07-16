cc119a4fcac10a835b6d5e69e497b163ef21b182
Amish Shah <amishshah.2k@gmail.com>
2020-07-16 22:16:37 +0100

    test: isVerified middleware

--- src/routes/middleware/isVerified.ts
+++ src/routes/middleware/isVerified.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Request } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import { AuthenticatedResponse } from './getUser';
 import { AccountStatus } from '../../entities/User';
 
@@ -6,8 +6,9 @@ enum VerifiedError {
 	Unverified = 'You are unverified to use this endpoint - please confirm your email address.'
 }
 
-export default function isVerified(req: Request, res: AuthenticatedResponse, next: NextFunction) {
-	if (res.locals.user.accountStatus !== AccountStatus.Verified) {
+export default function isVerified(req: Request, res: Response, next: NextFunction) {
+	if (!res.locals.user) return next(new Error(VerifiedError.Unverified));
+	if ((res as AuthenticatedResponse).locals.user.accountStatus !== AccountStatus.Verified) {
 		return next(new Error(VerifiedError.Unverified));
 	}
 	next();
