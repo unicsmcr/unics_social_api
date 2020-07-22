8684ed0378f433769c1e3a0dd1145056f5a0105e
Amish Shah <amishshah.2k@gmail.com>
2020-07-22 17:03:32 +0100

    feat: set up simple WebSocket server

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,19 @@
 import { createApp } from '.';
 import { getConfig } from './util/config';
+import { createServer } from 'http';
+import { Server as WebSocketServer } from 'ws';
 
 createApp()
 	.then(app => {
 		const port = getConfig().port;
-		app.listen(port, () => {
+		const server = createServer(app);
+
+		const wss = new WebSocketServer({ server, path: '/api/v1/gateway' });
+		wss.on('connection', ws => {
+			ws.send(JSON.stringify({ message: 'hi' }));
+		});
+
+		server.listen(port, () => {
 			console.log(`Listening on port ${port}`);
 		});
 	})
