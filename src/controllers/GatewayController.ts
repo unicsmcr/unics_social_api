572435b00b792f3b8ca05d4a9955cd05a3b9b032
Amish Shah <amishshah.2k@gmail.com>
2020-07-22 18:36:02 +0100

    feat: add authenticate method

--- src/controllers/GatewayController.ts
+++ src/controllers/GatewayController.ts
@@ -1,24 +1,31 @@
 import { inject, singleton } from 'tsyringe';
-import GatewayService from '../services/GatewayService';
-import WebSocket, { Server as WebSocketServer, Data as WebSocketData } from 'ws';
+import GatewayService, { GatewayPacket, GatewayPacketType, AuthenticateGatewayPacket, HelloGatewayPacket } from '../services/GatewayService';
+import WebSocket, { Server as WebSocketServer, Data } from 'ws';
 import { User } from '../entities/User';
+import { UserService } from '../services/UserService';
+import { verifyJWT } from '../util/auth';
 
 @singleton()
 export default class GatewayController {
 	private readonly gatewayService: GatewayService;
+	private readonly userService: UserService;
 	private wss?: WebSocketServer;
-	private readonly authenticatedClients: Map<WebSocket, User>;
+	public readonly authenticatedClients: Map<WebSocket, User>;
 
-	public constructor(@inject(GatewayService) gatewayService: GatewayService) {
+	public constructor(@inject(GatewayService) gatewayService: GatewayService, @inject(UserService) userService: UserService) {
 		this.authenticatedClients = new Map();
 		this.gatewayService = gatewayService;
+		this.userService = userService;
 	}
 
 	public bindTo(wss: WebSocketServer) {
 		if (this.wss) throw new Error('WebSocketServer is already bound!');
 		this.wss = wss;
 		this.wss.on('connection', ws => {
-			ws.on('message', data => this.onMessage(ws, data));
+			ws.on('message', data => void this.onMessage(ws, data).catch(error => {
+				console.error(error);
+				ws.close();
+			}));
 			ws.on('close', (code, reason) => {
 				this.authenticatedClients.delete(ws);
 				console.log(code, reason);
@@ -26,7 +33,31 @@ export default class GatewayController {
 		});
 	}
 
-	private onMessage(ws: WebSocket, data: WebSocketData) {
-		console.log(data);
+	private onMessage(ws: WebSocket, data: Data) {
+		const packet = this.gatewayService.parseIncoming(data);
+		return this.onPacket(ws, packet);
+	}
+
+	private async onPacket(ws: WebSocket, packet: GatewayPacket) {
+		if (packet.t === GatewayPacketType.Authenticate) {
+			await this.onAuthenticate(ws, packet as AuthenticateGatewayPacket);
+		}
+	}
+
+	public async onAuthenticate(ws: WebSocket, packet: AuthenticateGatewayPacket) {
+		let user = this.authenticatedClients.get(ws);
+		if (user) throw new Error('Already authenticated!');
+		const { token } = packet.d;
+		const { id } = await verifyJWT(token);
+		user = await this.userService.findOne({ id });
+		if (!user) throw new Error('User not found');
+		this.authenticatedClients.set(ws, user);
+		await this.gatewayService.send([ws], {
+			t: GatewayPacketType.Hello,
+			d: {
+				time: new Date().toISOString()
+			}
+		} as HelloGatewayPacket);
+		return user;
 	}
 }
--- src/services/GatewayService.ts
+++ src/services/GatewayService.ts
@@ -1,11 +1,50 @@
-import { injectable, inject } from 'tsyringe';
-import { UserService } from './UserService';
+import { injectable } from 'tsyringe';
+import WebSocket, { Data } from 'ws';
+
+export enum GatewayPacketType {
+	Authenticate,
+	Hello
+}
+
+export interface GatewayPacket {
+	t: GatewayPacketType;
+}
+
+export interface AuthenticateGatewayPacket extends GatewayPacket {
+	t: GatewayPacketType.Authenticate;
+	d: {
+		token: string;
+	};
+}
+
+export interface HelloGatewayPacket extends GatewayPacket {
+	t: GatewayPacketType.Hello;
+	d: {
+		time: string;
+	};
+}
+
 
 @injectable()
 export default class GatewayService {
-	private readonly userService: UserService;
+	public parseIncoming(data: Data): GatewayPacket {
+		if (Buffer.isBuffer(data)) {
+			data = data.toString();
+		}
+		if (typeof data !== 'string') {
+			throw new Error('Invalid incoming message type');
+		}
+		return JSON.parse(data);
+	}
 
-	public constructor(@inject(UserService) userService: UserService) {
-		this.userService = userService;
+	private sendData(client: WebSocket, data: string): Promise<void> {
+		return new Promise((resolve, reject) => {
+			client.send(data, error => error ? reject(error) : resolve(error));
+		});
+	}
+
+	public send(clients: WebSocket[], packet: GatewayPacket) {
+		const data = JSON.stringify(packet);
+		return Promise.all(clients.map(client => this.sendData(client, data)));
 	}
 }
