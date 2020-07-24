4c622e5619076c77a51eb0a7e98b28eed999484d
Amish Shah <amishshah.2k@gmail.com>
2020-07-24 21:52:35 +0100

    refactor: use APIError in EventService

--- src/services/EventService.ts
+++ src/services/EventService.ts
@@ -2,12 +2,13 @@ import { singleton } from 'tsyringe';
 import { Event } from '../entities/Event';
 import { getRepository, getConnection } from 'typeorm';
 import { validateOrReject } from 'class-validator';
-import { formatValidationErrors } from '../util/errors';
+import { formatValidationErrors, APIError } from '../util/errors';
 
 type EventCreationData = Omit<Event, 'id'>;
 
 enum PatchEventError {
-	IdMissing = 'Event ID missing'
+	IdMissing = 'Event ID missing',
+	EventNotFound = 'Event does not exist'
 }
 
 @singleton()
@@ -26,8 +27,8 @@ export default class EventService {
 
 	public async patchEvent(data: Pick<Event, 'id'> & Partial<Event>): Promise<Event> {
 		return getConnection().transaction(async entityManager => {
-			if (!data.id) throw new Error(PatchEventError.IdMissing);
-			const event = await entityManager.findOneOrFail(Event, data.id);
+			if (!data.id) throw new APIError(400, PatchEventError.IdMissing);
+			const event = await entityManager.findOneOrFail(Event, data.id).catch(() => Promise.reject(new APIError(400, PatchEventError.EventNotFound)));
 			Object.assign(event, data);
 			await validateOrReject(event).catch(e => Promise.reject(formatValidationErrors(e)));
 			return entityManager.save(event);
