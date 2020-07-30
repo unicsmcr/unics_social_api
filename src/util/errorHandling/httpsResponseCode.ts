64c168b35ce5c7137a901489a31dfca6ce0172d2
j95399rk <reean.m@gmail.com>
2020-07-30 14:14:42 +0100

    refactor: added enums for http response codes. Created a new directory called errorHandling and placed them there.

---
+++ src/util/errorHandling/httpsResponseCode.ts
@@ -0,0 +1,11 @@
+export enum HttpResponseCode {
+    OK = 200,
+    Created = 201,
+    Redirect = 302,
+    BadRequest = 400,
+    Unauthorized = 401,
+    Forbidden = 403,
+    NotFound = 404,
+    InternalError = 500,
+    NotImplemented = 501
+  }
