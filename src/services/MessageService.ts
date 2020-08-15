0102106c1238b5470a9e7da808b24143ef9924cd
Amish Shah <amishshah.2k@gmail.com>
2020-08-15 13:38:01 +0100

    feat(channels): add lastUpdated property to channels

--- src/services/MessageService.ts
+++ src/services/MessageService.ts
@@ -1,6 +1,6 @@
 import { singleton } from 'tsyringe';
 import Message, { APIMessage } from '../entities/Message';
-import { getRepository } from 'typeorm';
+import { getRepository, getConnection } from 'typeorm';
 import { APIError, formatValidationErrors, HttpCode } from '../util/errors';
 import { validateOrReject } from 'class-validator';
 import { Channel } from '../entities/Channel';
@@ -21,14 +21,18 @@ enum DeleteMessageError {
 
 @singleton()
 export default class MessageService {
-	public async createMessage(data: MessageCreationData): Promise<APIMessage> {
-		const message = new Message();
-		message.content = data.content;
-		message.time = new Date(data.time);
-		message.author = data.author;
-		message.channel = data.channel;
-		await validateOrReject(message).catch(e => Promise.reject(formatValidationErrors(e)));
-		return (await getRepository(Message).save(message)).toJSON();
+	public createMessage(data: MessageCreationData): Promise<APIMessage> {
+		return getConnection().transaction(async entityManager => {
+			const message = new Message();
+			message.content = data.content;
+			message.time = new Date(data.time);
+			message.author = data.author;
+			message.channel = data.channel;
+			await validateOrReject(message).catch(e => Promise.reject(formatValidationErrors(e)));
+			const savedMessage = await entityManager.save(message);
+			await entityManager.update(Channel, data.channel.id, { lastUpdated: message.time });
+			return savedMessage.toJSON();
+		});
 	}
 
 	public async getMessage(data: Pick<APIMessage, 'id' | 'channelID'>): Promise<APIMessage> {
@@ -50,12 +54,15 @@ export default class MessageService {
 		return messages.map(message => message.toJSON());
 	}
 
-	public async deleteMessage(data: Pick<APIMessage, 'id' | 'channelID'> & { authorID?: string }): Promise<void> {
-		if (!data.id) throw new APIError(HttpCode.NotFound, DeleteMessageError.NotFound);
-		const message = await getRepository(Message).findOneOrFail(data.id).catch(() => Promise.reject(new APIError(HttpCode.NotFound, DeleteMessageError.NotFound)));
-		if (message.channel.id !== data.channelID) throw new APIError(HttpCode.BadRequest, DeleteMessageError.InvalidChannel);
-		// If authorID is omitted, it means that the user has admin rights and does not need to provide their ID
-		if (data.authorID && message.author.id !== data.authorID) throw new APIError(HttpCode.BadRequest, DeleteMessageError.NotAuthor);
-		await getRepository(Message).delete(message);
+	public deleteMessage(data: Pick<APIMessage, 'id' | 'channelID'> & { authorID?: string }): Promise<void> {
+		return getConnection().transaction(async entityManager => {
+			if (!data.id) throw new APIError(HttpCode.NotFound, DeleteMessageError.NotFound);
+			const message = await entityManager.findOneOrFail(Message, data.id).catch(() => Promise.reject(new APIError(HttpCode.NotFound, DeleteMessageError.NotFound)));
+			if (message.channel.id !== data.channelID) throw new APIError(HttpCode.BadRequest, DeleteMessageError.InvalidChannel);
+			// If authorID is omitted, it means that the user has admin rights and does not need to provide their ID
+			if (data.authorID && message.author.id !== data.authorID) throw new APIError(HttpCode.BadRequest, DeleteMessageError.NotAuthor);
+			await entityManager.delete(Message, message.id);
+			await entityManager.update(Channel, message.channel.id, { lastUpdated: new Date() });
+		});
 	}
 }
