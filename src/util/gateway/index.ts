fa66d8ac1a686ce14fa1209849f34c7d3e609c5b
Amish Shah <amishshah.2k@gmail.com>
2020-08-16 19:41:57 +0100

    feat: implement discovery queue (#53)
    * feat: initial discovery queue
    
    * feat: start basic discovery queue gateway packets
    
    * feat: add APIUser and APIPrivateUser
    
    * refactor(gateway): include user in queue match packet
    
    * fix: duplicate user in queue check
    
    * refactor(discovery): inject services in constructor
    
    * feat(gateway): integrate discovery queue into gateway
    
    * fix(gateway): handle discovery errors

--- src/util/gateway/index.ts
+++ src/util/gateway/index.ts
@@ -1,4 +1,6 @@
 import { APIMessage } from '../../entities/Message';
+import { QueueOptions } from '../discovery/DiscoveryQueue';
+import { APIDMChannel } from '../../entities/Channel';
 
 export class GatewayError extends Error {}
 
@@ -6,7 +8,13 @@ export enum GatewayPacketType {
 	Identify = 'IDENTIFY',
 	Hello = 'HELLO',
 	MessageCreate = 'MESSAGE_CREATE',
-	MessageDelete = 'MESSAGE_DELETE'
+	MessageDelete = 'MESSAGE_DELETE',
+	// Sent by the client to join a Discovery queue
+	JoinDiscoveryQueue = 'JOIN_DISCOVERY_QUEUE',
+	// Sent by the client to leave a Discovery queue
+	LeaveDiscoveryQueue = 'LEAVE_DISCOVERY_QUEUE',
+	// Sent by the gateway once a match has been made
+	DiscoveryQueueMatch = 'DISCOVERY_QUEUE_MATCH'
 }
 
 export interface GatewayPacket {
@@ -36,5 +44,23 @@ export interface MessageDeleteGatewayPacket extends GatewayPacket {
 	data: {
 		messageID: string;
 		channelID: string;
+	};
+}
+
+export interface JoinDiscoveryQueuePacket extends GatewayPacket {
+	type: GatewayPacketType.JoinDiscoveryQueue;
+	data: {
+		options: QueueOptions;
+	};
+}
+
+export interface LeaveDiscoveryQueuePacket extends GatewayPacket {
+	type: GatewayPacketType.LeaveDiscoveryQueue;
+}
+
+export interface DiscoveryQueueMatchPacket extends GatewayPacket {
+	type: GatewayPacketType.DiscoveryQueueMatch;
+	data: {
+		channel: APIDMChannel;
 	};
 }
