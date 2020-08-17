d6f5bf4b37c968abbef137a59a340997e4ac72f2
Amish Shah <amishshah.2k@gmail.com>
2020-08-17 17:41:20 +0100

    feat: create/complete room twilio methods

--- src/services/TwilioService.ts
+++ src/services/TwilioService.ts
@@ -9,4 +9,21 @@ export class TwilioService {
 		const { twilio } = getConfig();
 		this.twilioClient = buildClient(twilio.accountSid, twilio.token);
 	}
+
+	public async createRoom(roomId: string): Promise<string> {
+		const room = await this.twilioClient.video.rooms.create({
+			enableTurn: true,
+			type: 'peer-to-peer',
+			uniqueName: roomId,
+			maxParticipants: 2
+		});
+		// We can use the given roomId to access the room in place of its actual SID
+		return room.uniqueName;
+	}
+
+	public async completeRoom(roomId: string): Promise<void> {
+		await this.twilioClient.video.rooms(roomId).update({
+			status: 'completed'
+		});
+	}
 }
