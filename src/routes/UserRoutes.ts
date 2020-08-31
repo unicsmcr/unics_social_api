db76aeaeb38ba01aca7fa4637f8d64ab3c6252fe
Shahed Abu Qamar <57857104+sabuqamar@users.noreply.github.com>
2020-08-31 09:50:49 +0100

    feat: images for events (#69)
    * feat: allow event to add image
    
    * feat: allow event to add image
    
    * refactor: Upload avatar through middleware
    
    * fix: uploading events
    
    * fix: uploading images on new event
    
    * fix: tests
    
    Co-authored-by: Amish Shah <amishshah.2k@gmail.com>

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -1,17 +1,7 @@
 import { Router } from 'express';
 import { UserController } from '../controllers/UserController';
 import { inject, injectable } from 'tsyringe';
-import { getUser, isVerified } from './middleware';
-import multer from 'multer';
-import { APIError, HttpCode } from '../util/errors';
-
-const storage = multer.memoryStorage();
-const upload = multer({
-	storage,
-	limits: {
-		fileSize: 2 ** 19 // 512 KB file limit
-	}
-});
+import { getUser, isVerified, uploadImg } from './middleware';
 
 @injectable()
 export class UserRoutes {
@@ -30,16 +20,7 @@ export class UserRoutes {
 
 		router.get('/users/:id', getUser, isVerified, this.userController.getUser.bind(this.userController));
 
-		router.put('/users/@me/profile', getUser, isVerified, (req, res, next) => {
-			upload.single('avatar')(req, res, (err?: any) => {
-				if (err instanceof multer.MulterError) {
-					return next(new APIError(HttpCode.BadRequest, err.message));
-				} else if (err) {
-					return next(err);
-				}
-				return next();
-			});
-		}, this.userController.putUserProfile.bind(this.userController));
+		router.put('/users/@me/profile', getUser, isVerified, uploadImg('avatar'), this.userController.putUserProfile.bind(this.userController));
 
 		router.post('/users/:recipientID/channel', getUser, isVerified, this.userController.createDMChannel.bind(this.userController));
 	}
--- src/routes/middleware/index.ts
+++ src/routes/middleware/index.ts
@@ -2,3 +2,4 @@ export { default as getUser } from './getUser';
 export { default as isVerified } from './isVerified';
 export { default as isAdmin } from './isAdmin';
 export { default as getChannel } from './getChannel';
+export { default as uploadImg } from './uploadImg';
