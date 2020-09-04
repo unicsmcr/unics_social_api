2e791a8c49232cbd97bf4feb2adba01ca450b421
graphtheoryiscool <kashishraimalani@gmail.com>
2020-09-04 12:46:46 +0530

    removes comment

--- src/logger.ts
+++ src/logger.ts
@@ -2,6 +2,3 @@ import { default as pino } from 'pino';
 export const logger = pino({
 	prettyPrint: true
 });
-
-/* const expressPino = require('express-pino-logger');
-const myExpressLogger = expressPino({ myLogger }) */
