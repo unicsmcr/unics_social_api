d8333d9bcff3ae09068dcf611baf2effc01c32ee
Amish Shah <amishshah.2k@gmail.com>
2020-09-12 13:33:37 +0100

    feat: set minimum length for report

--- src/entities/Report.ts
+++ src/entities/Report.ts
@@ -1,5 +1,5 @@
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinColumn } from 'typeorm';
-import { IsDate, MaxLength, IsString } from 'class-validator';
+import { IsDate, MaxLength, IsString, MinLength } from 'class-validator';
 import { User } from './User';
 
 export interface APIReport {
@@ -29,6 +29,7 @@ export default class Report {
 
 	@Column()
 	@IsString()
+	@MinLength(10, { message: 'A report description must be at least 10 characters' })
 	@MaxLength(2000, { message: 'A report description must be 2000 characters at most' })
 	public description!: string;
 
