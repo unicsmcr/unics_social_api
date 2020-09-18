326d75d1c3ecc0a9192a5d729b1482b41bf347fc
Amish Shah <amishshah.2k@gmail.com>
2020-09-18 21:26:10 +0100

    fix: use correct name for route

--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -17,7 +17,7 @@ export class UserRoutes {
 
 		router.get('/verify', getUser(TokenType.EmailVerify), this.userController.verifyUserEmail.bind(this.userController));
 
-		router.post('/forgotEmail', this.userController.resendVerificationEmail.bind(this.userController));
+		router.post('/resendVerificationEmail', this.userController.resendVerificationEmail.bind(this.userController));
 
 		router.post('/authenticate', this.userController.authenticate.bind(this.userController));
 
