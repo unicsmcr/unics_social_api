a632de99a7cd919896d624734c7bd73217ee0e67
Amish Shah <amishshah.2k@gmail.com>
2020-09-26 18:43:30 +0100

    fix: file size limit in error messages

--- src/routes/middleware/uploadImg.ts
+++ src/routes/middleware/uploadImg.ts
@@ -14,7 +14,8 @@ export default function uploadImg(fileName: string) {
 	return (req: Request, res: Response, next: NextFunction) => {
 		upload.single(fileName)(req, res, (err?: any) => {
 			if (err instanceof multer.MulterError) {
-				return next(new APIError(HttpCode.BadRequest, err.message));
+				const message = err.code === 'LIMIT_FILE_SIZE' ? 'File too large (1MB limit)' : err.message;
+				return next(new APIError(HttpCode.BadRequest, message));
 			} else if (err) {
 				return next(err);
 			}
