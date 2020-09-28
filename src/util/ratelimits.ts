79b3cd999a93795d80d0fea9dc508225fab7731d
Amish Shah <amishshah.2k@gmail.com>
2020-09-28 09:39:48 +0100

    add debug message

--- src/util/ratelimits.ts
+++ src/util/ratelimits.ts
@@ -1,4 +1,5 @@
 import rateLimit from 'express-rate-limit';
+import { Request } from 'express';
 
 const SECOND = 1e3;
 const MINUTE = 60 * SECOND;
@@ -8,38 +9,49 @@ const message: any = {
 	error: 'You are being ratelimited! Try again in a few minutes.'
 };
 
+const keyGenerator = (req: Request) => {
+	console.log(req.ip, req.headers);
+	return req.ip;
+};
+
 export const registerRateLimit = rateLimit({
 	windowMs: HOUR,
 	max: 3,
-	message
+	message,
+	keyGenerator
 });
 
 export const sendMessageRateLimit = rateLimit({
 	windowMs: 10 * SECOND,
 	max: 10,
-	message
+	message,
+	keyGenerator
 });
 
 export const resendVerificationRateLimit = rateLimit({
 	windowMs: 5 * MINUTE,
 	max: 1,
-	message
+	message,
+	keyGenerator
 });
 
 export const forgotPasswordRateLimit = rateLimit({
 	windowMs: 5 * MINUTE,
 	max: 2,
-	message
+	message,
+	keyGenerator
 });
 
 export const reportRateLimit = rateLimit({
 	windowMs: MINUTE,
 	max: 3,
-	message
+	message,
+	keyGenerator
 });
 
 export const discordRateLimit = rateLimit({
 	windowMs: MINUTE,
 	max: 2,
-	message
+	message,
+	keyGenerator
 });
