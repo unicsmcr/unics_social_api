bb6525319fa55781acc3983efea0cce7410285c8
Amish Shah <amishshah.2k@gmail.com>
2020-10-18 15:25:02 +0100

    refactor: update ratelimiting to allow for passthrough

--- src/routes/ChannelRoutes.ts
+++ src/routes/ChannelRoutes.ts
@@ -5,7 +5,7 @@ import { MessageController } from '../controllers/MessageController';
 import getChannel from './middleware/getChannel';
 import { UserController } from '../controllers/UserController';
 import { TokenType } from '../util/auth';
-import { sendMessageRateLimit } from '../util/ratelimits';
+import { getRatelimiter, RateLimiter } from '../util/ratelimits';
 
 @injectable()
 export class ChannelRoutes {
@@ -19,7 +19,7 @@ export class ChannelRoutes {
 
 	public routes(router: Router): void {
 		router.get('/channels', getUser(TokenType.Auth), isVerified, this.userController.getChannels.bind(this.userController));
-		router.post('/channels/:channelID/messages', sendMessageRateLimit, getUser(TokenType.Auth), isVerified, getChannel, this.messageController.createMessage.bind(this.messageController));
+		router.post('/channels/:channelID/messages', getRatelimiter(RateLimiter.SendMessage), getUser(TokenType.Auth), isVerified, getChannel, this.messageController.createMessage.bind(this.messageController));
 		router.get('/channels/:channelID/messages', getUser(TokenType.Auth), isVerified, getChannel, this.messageController.getMessages.bind(this.messageController));
 		router.get('/channels/:channelID/messages/:messageID', getUser(TokenType.Auth), isVerified, this.messageController.getMessage.bind(this.messageController));
 		router.delete('/channels/:channelID/messages/:messageID', getUser(TokenType.Auth), isVerified, getChannel, this.messageController.deleteMessage.bind(this.messageController));
--- src/routes/UserRoutes.ts
+++ src/routes/UserRoutes.ts
@@ -4,7 +4,7 @@ import { inject, injectable } from 'tsyringe';
 import { getUser, isVerified, uploadImg } from './middleware';
 import { TokenType } from '../util/auth';
 import { DiscordController } from '../controllers/DiscordController';
-import { registerRateLimit, discordRateLimit, reportRateLimit, resendVerificationRateLimit, forgotPasswordRateLimit } from '../util/ratelimits';
+import { getRatelimiter, RateLimiter } from '../util/ratelimits';
 
 @injectable()
 export class UserRoutes {
@@ -17,15 +17,15 @@ export class UserRoutes {
 	}
 
 	public routes(router: Router): void {
-		router.post('/register', registerRateLimit, this.userController.registerUser.bind(this.userController));
+		router.post('/register', getRatelimiter(RateLimiter.Register), this.userController.registerUser.bind(this.userController));
 
 		router.get('/verify', getUser(TokenType.EmailVerify), this.userController.verifyUserEmail.bind(this.userController));
 
-		router.post('/resendVerificationEmail', resendVerificationRateLimit, this.userController.resendVerificationEmail.bind(this.userController));
+		router.post('/resendVerificationEmail', getRatelimiter(RateLimiter.ResendVerificationEmail), this.userController.resendVerificationEmail.bind(this.userController));
 
 		router.post('/authenticate', this.userController.authenticate.bind(this.userController));
 
-		router.post('/forgot_password', forgotPasswordRateLimit, this.userController.forgotPassword.bind(this.userController));
+		router.post('/forgot_password', getRatelimiter(RateLimiter.ForgotPassword), this.userController.forgotPassword.bind(this.userController));
 
 		router.post('/reset_password', getUser(TokenType.PasswordReset), this.userController.resetPassword.bind(this.userController));
 
@@ -39,13 +39,13 @@ export class UserRoutes {
 
 		router.delete('/users/@me/notes/:id', getUser(TokenType.Auth), isVerified, this.userController.deleteNote.bind(this.userController));
 
-		router.post('/users/:id/report', reportRateLimit, getUser(TokenType.Auth), isVerified, this.userController.reportUser.bind(this.userController));
+		router.post('/users/:id/report', getRatelimiter(RateLimiter.Report), getUser(TokenType.Auth), isVerified, this.userController.reportUser.bind(this.userController));
 
 		router.put('/users/@me/profile', getUser(TokenType.Auth), isVerified, uploadImg('avatar'), this.userController.putUserProfile.bind(this.userController));
 
 		router.get('/users/@me/discord/authorize', getUser(TokenType.Auth), isVerified, this.discordController.getOAuth2AuthorizeURL.bind(this.discordController));
 
-		router.post('/users/@me/discord/link', discordRateLimit, getUser(TokenType.Auth), this.discordController.linkAccount.bind(this.discordController));
+		router.post('/users/@me/discord/link', getRatelimiter(RateLimiter.Discord), getUser(TokenType.Auth), this.discordController.linkAccount.bind(this.discordController));
 
 		router.post('/users/:recipientID/channel', getUser(TokenType.Auth), isVerified, this.userController.createDMChannel.bind(this.userController));
 	}
