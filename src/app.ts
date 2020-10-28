449fd5f7a1dc85b3ebebf0659bbaf7b0dba61933
Amish Shah <amishshah.2k@gmail.com>
2020-10-28 20:34:04 +0000

    feat: log environment to console

--- src/app.ts
+++ src/app.ts
@@ -6,14 +6,14 @@ import { logger } from './util/logger';
 
 createApp()
 	.then(app => {
-		const port = getConfig().port;
+		const { port, env } = getConfig();
 		const server = createServer(app);
 
 		const wss = new WebSocketServer({ server, path: '/api/v1/gateway' });
 		createGateway(wss);
 
 		server.listen(port, () => {
-			logger.info(`Listening on port ${port}`);
+			logger.info(`Listening on port ${port}. Running in ${env} environment.`);
 		});
 	})
 	.catch(err => logger.error(err));
